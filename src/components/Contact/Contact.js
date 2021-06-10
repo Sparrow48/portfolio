4c910a6e69fc58f1932536a52f6b436bf5041a7c
Sparrow48 <abunasib48@gmail.com>
2021-06-10 23:44:03 +0600

    add contact section

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,42 +7,43 @@ import classes from "./Contact.module.css";
 
 const Contact = () => {
   return (
-    <section id='contact' className={`${classes.contact}`}>
+    <section id='contact' className={classes.contact}>
       <Fade bottom duration={2500} distance='40px'>
         <Container>
           <div className='text-center'>
             <h1>contact us</h1>
-            <h4>We'd love to hear from you!</h4>
+            <h4>Plese leave a message, in case you have any query.</h4>
           </div>
-          <form className='py-5'>
-            <Row className='input-container'>
+          <form>
+            <Row className={classes.row}>
               <Col md={6} sm={12}>
-                <div className='styled-input wide'>
+                <div className={classes.Input}>
                   <input type='text' required />
                   <label>Name</label>
                 </div>
               </Col>
               <Col md={6} sm={12}>
-                <div className='styled-input' style={{ float: "right" }}>
+                <div className={classes.Input} style={{ float: "right" }}>
                   <input type='text' required />
                   <label>Email</label>
                 </div>
               </Col>
               <Col xs={12}>
-                <div className='styled-input'>
+                <div className={`${classes.Input} ${classes.wideArea}`}>
                   <input type='text' required />
                   <label>Mail Subject</label>
                 </div>
               </Col>
+
               <Col xs={12}>
-                <div className='styled-input wide'>
+                <div className={`${classes.Input} ${classes.wideArea}`}>
                   <textarea required />
                   <label>Message</label>
                 </div>
               </Col>
               <Col xs={12}>
-                <button className='btn-lrg submit-btn btn-main'>
-                  Send Message
+                <button className={`${classes["submit-btn"]}`}>
+                  Send Mail
                 </button>
               </Col>
             </Row>
